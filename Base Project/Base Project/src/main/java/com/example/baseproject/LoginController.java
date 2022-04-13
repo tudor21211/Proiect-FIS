c8201d588dd981425b55398afe9605fab24d1cc6
tudor21211 <tudor.androne@student.upt.ro>
2022-04-13 18:31:01 +0300

    added log in scene view

--- Base Project/Base Project/src/main/java/com/example/baseproject/LoginController.java
+++ Base Project/Base Project/src/main/java/com/example/baseproject/LoginController.java
@@ -1,17 +1,55 @@
 package com.example.baseproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
+import java.io.File;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class LoginController {
+public class LoginController /*implements Initializable*/ {
     @FXML
     private Label welcomeText;
 
     private Button LoginButton;
+ @FXML
+    private Label invalidLogin;
+ @FXML
+ private ImageView brandingImageView;
+ @FXML
+ private ImageView lockImageView;
+ @FXML
+ private Button cancelButton;
+
+
+
+
+ //@Override
+ /*public void initialize (URL url, ResourceBundle resourceBundle) {
+     File branding = new File("img.png");
+     Image brandingImage = new Image(branding.toURI().toString());
+     brandingImageView.setImage(brandingImage);
+
+     File lock = new File("Lock.png");
+     Image lockImage = new Image(lock.toURI().toString());
+     lockImageView.setImage(lockImage);
+
+ }
+*/
+    public void loginButtonOnAction(ActionEvent event) {
+        invalidLogin.setText("You try to log in...");
+    }
+
+    public void cancelButtonOnAction (ActionEvent event) {
+     Stage stage = (Stage) cancelButton.getScene().getWindow();
+     stage.close();
+    }
 
     @FXML
     protected void onHelloButtonClick() {
