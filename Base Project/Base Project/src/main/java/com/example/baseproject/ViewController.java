d3f24435f61c28c9a82bcf2d71f3dc03cb159de4
Albert Endre <albert_endre@yahoo.com>
2022-04-26 11:58:57 +0300

    created class and tried to create cancel button :(

---
+++ Base Project/Base Project/src/main/java/com/example/baseproject/ViewController.java
@@ -0,0 +1,24 @@
+package com.example.baseproject;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.PasswordAuthentication;
+import java.sql.*;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+
+public class ViewController {
+
+}
