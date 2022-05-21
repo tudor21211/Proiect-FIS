21ef9061fb72a24a10a0e7aeace0369c26c9e782
tudor21211 <tudor.androne@student.upt.ro>
2022-05-21 15:58:53 +0300

    added jar file

---
+++ Base Project/Base Project/src/main/java/com/example/baseproject/Main_1.java
@@ -0,0 +1,9 @@
+package com.example.baseproject;
+
+import com.example.baseproject.LoginApplication;
+
+public class Main_1 {
+    public static void main (String[] args) {
+        LoginApplication.main(args);
+    }
+}
